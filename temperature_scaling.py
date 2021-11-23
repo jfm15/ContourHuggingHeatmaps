fix temperature scaling

temperature_scaling.py
@@ -104,6 +104,9 @@ def main():
 
             losses_per_epoch.append(loss.item())
 
+            if (batch + 1) % 5 == 0:
+                logger.info("[{}/{}]\tLoss: {:.3f}".format(batch + 1, len(training_loader), np.mean(losses_per_epoch)))
+
         msg = "Loss: {:.3f}".format(np.mean(losses_per_epoch))
         logger.info(msg)
 
@@ -128,9 +131,9 @@ def main():
 
                 # Get the radial/localisation error and expected radial error values for each heatmap
                 radial_errors, _, mode_probabilities \
-                    = evaluate(output.detach().numpy(),
-                               meta['landmarks_per_annotator'].detach().numpy(),
-                               meta['pixel_size'].detach().numpy())
+                    = evaluate(output.cpu().detach().numpy(),
+                               meta['landmarks_per_annotator'].cpu().detach().numpy(),
+                               meta['pixel_size'].cpu().detach().numpy())
                 validation_radial_errors.append(radial_errors)
                 validation_mode_probabilities.append(mode_probabilities)
 

train.py
@@ -60,8 +60,8 @@ def main():
     logger.info("")
 
     # load the train dataset and put it into a loader
-    train_dataset = LandmarkDataset(args.training_images, args.annotations, cfg.DATASET, perform_augmentation=True)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=cfg.TRAIN.BATCH_SIZE, shuffle=True)
+    training_dataset = LandmarkDataset(args.training_images, args.annotations, cfg.DATASET, perform_augmentation=True)
+    training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=cfg.TRAIN.BATCH_SIZE, shuffle=True)
 
     '''
     for batch, (image, channels, meta) in enumerate(train_loader):
@@ -93,7 +93,7 @@ def main():
         model.train()
         losses_per_epoch = []
 
-        for batch, (image, channels, meta) in enumerate(train_loader):
+        for batch, (image, channels, meta) in enumerate(training_loader):
 
             # Put image and channels onto gpu
             image = image.cuda()
@@ -111,7 +111,7 @@ def main():
             losses_per_epoch.append(loss.item())
 
             if (batch + 1) % 5 == 0:
-                logger.info("[{}/{}]\tLoss: {:.3f}".format(batch + 1, len(train_loader), np.mean(losses_per_epoch)))
+                logger.info("[{}/{}]\tLoss: {:.3f}".format(batch + 1, len(training_loader), np.mean(losses_per_epoch)))
 
         scheduler.step()
 